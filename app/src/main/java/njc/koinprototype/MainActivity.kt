9f70272bf958b7d30227b5a311b4fb761a56cc97
Nick Cipollo <nick.cipollo@thelevelup.com>
2018-09-24 16:52:38 -0400

    Add initial project.

---
+++ app/src/main/java/njc/koinprototype/MainActivity.kt
@@ -0,0 +1,12 @@
+package njc.koinprototype
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
